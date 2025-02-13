965786e6a49d258bae6e41ca72850cf6f03c1cfc
Daniel Sampford <danielsampford1@gmail.com>
2025-02-13 20:15:07 +0000

    BP Character Movement

---
+++ Source/FirstProject/Public/Core/FPGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API AFPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
